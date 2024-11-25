dcf9e738e671aa22b52f49da5bf359ee7ff3f86a
Vaibhavthestudent <1ep22cs116@eastpoint.ac.in>
2024-11-25 19:49:18 +0530

    Removed Firebase Creds

--- App.js
+++ App.js
@@ -110,24 +110,3 @@ export default function App() {
 }
 
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC9gZrXVvdMPyhqVxTWXFIgs5KhGUmqOUk",
-//   authDomain: "notification-8f388.firebaseapp.com",
-//   projectId: "notification-8f388",
-//   storageBucket: "notification-8f388.firebasestorage.app",
-//   messagingSenderId: "736394516255",
-//   appId: "1:736394516255:web:ccb1048c5661406144822a",
-//   measurementId: "G-Y3TVFRCBPB"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
